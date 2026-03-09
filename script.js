669a1dd9a3849b7440b8b29290a288d259f5aded
bichron <chrispham.pnb@gmail.com>
2026-03-09 23:36:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -322,7 +322,62 @@ function updateQR(slider){
 
 }
 
+/* ===========================
+   LOAD GALLERY
+=========================== */
+function loadAchievementSlider(slider){
 
+  const track = slider.querySelector(".achievement-track");
+  const indicatorBox = slider.querySelector(".achievement-indicators");
+
+  const group = slider.dataset.group;
+  const maxAllowed = parseInt(slider.dataset.max);
+
+  track.innerHTML="";
+  indicatorBox.innerHTML="";
+
+  let images=[];
+  let index=1;
+
+  function tryLoad(){
+
+    const img=new Image();
+    img.src=`assets/achievement/${group}/${index}.jpg`;
+
+    img.onload=()=>{
+      images.push(img.src);
+      index++;
+      tryLoad();
+    };
+
+    img.onerror=build;
+
+  }
+
+  function build(){
+
+    const count=Math.min(maxAllowed,images.length);
+
+    for(let i=0;i<count;i++){
+
+      const el=document.createElement("img");
+      el.src=images[i];
+
+      track.appendChild(el);
+
+      const dot=document.createElement("span");
+      if(i===0) dot.classList.add("active");
+
+      indicatorBox.appendChild(dot);
+
+    }
+
+  }
+
+  tryLoad();
+
+}
+   
 /* ===========================
    QR ZOOM
 =========================== */
@@ -414,6 +469,10 @@ document
   if(!achievementWheel) initAchievementWheel();
 
   achievementWheel.go(0);
+
+  document
+  .querySelectorAll("#achievementPopup .achievement-slider")
+  .forEach(loadAchievementSlider);
 
   openPopup("achievementPopup");
 
