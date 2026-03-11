f84d86778f6f539dfa2eaa36394bac9ea20a5975
bichron <chrispham.pnb@gmail.com>
2026-03-11 17:29:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1072,26 +1072,4 @@ function closeLandingpage(){
 
 window.openWebsite=()=>window.open("https://blh.vn","_blank");
 
-
-window.downloadVCF=()=>{
-
-  const vcf=`BEGIN:VCARD
-VERSION:3.0
-FN:Chris Pham
-TITLE:Chief Commercial Officer
-ORG:BLH Joint Stock Company
-TEL:0909554558
-END:VCARD`;
-
-  const blob=new Blob([vcf],{type:"text/vcard"});
-
-  const a=document.createElement("a");
-
-  a.href=URL.createObjectURL(blob);
-  a.download="Chris_Pham.vcf";
-
-  a.click();
-
-};
-
 });
