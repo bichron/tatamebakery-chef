68dd59b06dee51309260c4c6e5358ab48c6bff62
bichron <chrispham.pnb@gmail.com>
2026-03-13 00:44:14 +0700

    Add files via upload

---
+++ cart/cartengine.js
@@ -0,0 +1,50 @@
+
+/* ===========================
+   CART ENGINE
+=========================== */
+
+import { AppState } from "./state.js";
+
+export const CartEngine = {
+
+  add(id){
+
+    if(!AppState.cart[id]){
+
+      AppState.cart[id] = 1;
+
+    }else{
+
+      AppState.cart[id]++;
+
+    }
+
+  },
+
+  remove(id){
+
+    if(!AppState.cart[id]) return;
+
+    AppState.cart[id]--;
+
+    if(AppState.cart[id] <= 0){
+
+      delete AppState.cart[id];
+
+    }
+
+  },
+
+  clear(){
+
+    AppState.cart = {};
+
+  },
+
+  getItems(){
+
+    return AppState.cart;
+
+  }
+
+};
