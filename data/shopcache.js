85c9be040198d5309d60e90d31010858269b3b0c
bichron <chrispham.pnb@gmail.com>
2026-03-22 23:35:54 +0700

    Create shopcache.js

---
+++ data/shopcache.js
@@ -0,0 +1,13 @@
+const groupCache = new Map();
+
+export async function loadGroupData(groupId){
+  if(groupCache.has(groupId)){
+    return groupCache.get(groupId);
+  }
+
+  const res = await fetch(`./assets/shop/${groupId}.json`);
+  const data = await res.json();
+
+  groupCache.set(groupId, data);
+  return data;
+}
